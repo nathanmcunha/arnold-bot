e7007d3d14ca26f4719075b6f4b04bdb199ae15c
Nathan Cunha <nathanmartins@outlook.com>
2023-03-02 11:42:15 -0300

    add spring base project

---
+++ src/test/java/br/com/nathancunha/arnold/bot/ArnoldBotApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nathancunha.arnold.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArnoldBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
